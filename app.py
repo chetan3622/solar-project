98279e2acfe7e2a3053b9a0068ee37c36fd9b799
chetan <156156147+chetan3622@users.noreply.github.com>
2026-07-17 11:18:36 +0530

    Fix spacing in Prediction page condition

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ elif page == "📘About":
     </div>
     """, unsafe_allow_html=True)
 
-elif page == "⚡ Prediction":
+elif page == "⚡Prediction":
     st.markdown('<div class="glass-card">', unsafe_allow_html=True)
 
     st.subheader("Enter Weather Parameters")
